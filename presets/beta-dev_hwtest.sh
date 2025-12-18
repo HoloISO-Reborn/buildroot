2d0b654b0184984930fc25d5a06ac04dbc038346
xxcosita3czxx <kokosove18@gmail.com>
2025-12-18 14:06:39 +0100

    adding grub normally

--- presets/beta-dev_hwtest.sh
+++ presets/beta-dev_hwtest.sh
@@ -9,7 +9,7 @@ export OS_CODENAME="Beta"
 export OS_FS_PREFIX="holo"
 export RELEASETAG=snapshot$(date +%Y%m%d.%H%M.%S)
 echo -e ${RELEASETAG} > /tmp/builder-releasetag
-echo -e "$(echo ${DISTRO_NAME} | tr '[:upper:]' '[:lower:]')_$(echo ${OS_CODENAME} | tr '[:upper:]' '[:lower:]')_${RELEASETAG}-dev_hwtest" > /tmp/build_temp_ver
+echo -e "$(echo ${DISTRO_NAME} | tr '[:upper:]' '[:lower:]')_$#(echo ${OS_CODENAME} | tr '[:upper:]' '[:lower:]')_${RELEASETAG}-dev_hwtest" > /tmp/build_temp_ver
 export FLAVOR_BUILDVER=$(cat /tmp/build_temp_ver)
 export IMAGEFILE="${FLAVOR_BUILDVER}"
 export FLAVOR_CHROOT_SCRIPTS="sddm bluetooth sshd systemd-timesyncd NetworkManager"
